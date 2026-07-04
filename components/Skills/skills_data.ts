769e377199e5bc90df6e32c8f4f0f5c0fadb8b93
Dmytro Platov <90376194+Erebus1678@users.noreply.github.com>
2026-07-05 01:54:41 +0300

    feat(skills): expand skills grid to match current CV
    Add MUI, Styled Components, Storybook, Jest, Webpack, Vite, Git, GitLab
    (self-hosted devicon SVGs); rename Redux -> Redux Toolkit. 11 -> 19 skills.

--- components/Skills/skills_data.ts
+++ components/Skills/skills_data.ts
@@ -6,16 +6,28 @@ export const skills_data: Skill[] = [
     title: 'JavaScript',
   },
   {
+    image: '/Icons/typescript.svg',
+    title: 'TypeScript',
+  },
+  {
     image: '/Icons/react.svg',
     title: 'React',
   },
   {
     image: '/Icons/redux.svg',
-    title: 'Redux',
+    title: 'Redux Toolkit',
   },
   {
     image: '/Icons/next.png',
     title: 'Next.js',
+  },
+  {
+    image: '/Icons/html5.svg',
+    title: 'HTML 5',
+  },
+  {
+    image: '/Icons/css3.svg',
+    title: 'CSS 3',
   },
   {
     image: '/Icons/sass.svg',
@@ -26,20 +38,40 @@ export const skills_data: Skill[] = [
     title: 'TailwindCSS',
   },
   {
+    image: '/Icons/materialui.svg',
+    title: 'MUI',
+  },
+  {
+    image: '/Icons/styledcomponents.svg',
+    title: 'Styled Components',
+  },
+  {
     image: '/Icons/bootstrap.svg',
     title: 'Bootstrap',
   },
   {
-    image: '/Icons/css3.svg',
-    title: 'CSS 3',
+    image: '/Icons/storybook.svg',
+    title: 'Storybook',
   },
   {
-    image: '/Icons/html5.svg',
-    title: 'HTML 5',
+    image: '/Icons/jest.svg',
+    title: 'Jest',
   },
   {
-    image: '/Icons/typescript.svg',
-    title: 'TypeScript',
+    image: '/Icons/webpack.svg',
+    title: 'Webpack',
+  },
+  {
+    image: '/Icons/vite.svg',
+    title: 'Vite',
+  },
+  {
+    image: '/Icons/git.svg',
+    title: 'Git',
+  },
+  {
+    image: '/Icons/gitlab.svg',
+    title: 'GitLab',
   },
   {
     image: '/Icons/python.png',
